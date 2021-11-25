40ece01d656228095ab9b9568a6eca4bd6c8c745
Stefan Wiedemann <wistefan@googlemail.com>
2021-11-25 13:14:41 +0100

    fix endpoint retrieval

--- src/endpoint-configuration-service/src/main/java/org/fiware/sidecar/rest/AuthConfigurationApiController.java
+++ src/endpoint-configuration-service/src/main/java/org/fiware/sidecar/rest/AuthConfigurationApiController.java
@@ -1,5 +1,6 @@
 package org.fiware.sidecar.rest;
 
+import io.micronaut.core.util.AntPathMatcher;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import lombok.RequiredArgsConstructor;
@@ -9,19 +10,34 @@ import org.fiware.sidecar.mapping.EndpointMapper;
 import org.fiware.sidecar.model.AuthInfoVO;
 import org.fiware.sidecar.persistence.EndpointRepository;
 
+import java.util.Comparator;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class AuthConfigurationApiController implements AuthConfigurationApi {
 
+	private final PathPartMatcher pathPartMatcher = new PathPartMatcher();
+
 	private final EndpointRepository endpointRepository;
 	private final EndpointMapper endpointMapper;
 
+
 	@Override
 	public HttpResponse<AuthInfoVO> getEndpointByDomainAndPath(String domain, String path) {
-		return endpointRepository.findByDomainAndPath(domain, path)
+
+		return endpointRepository.findByDomain(domain).stream()
+				.filter(endpoint -> pathPartMatcher.matchesPartly(path, endpoint.getPath()))
+				// we want the longest match(e.g. /test instead of / when /test/path is asked)
+				.max(Comparator.comparingInt(endpoint -> endpoint.getPath().length()))
 				.map(endpointMapper::endpointToAuthInfoVo)
 				.map(HttpResponse::ok)
 				.orElse(HttpResponse.notFound());
+	}
+
+	private class PathPartMatcher extends AntPathMatcher {
+		public boolean matchesPartly(String pattern, String source) {
+			return doMatch(pattern, source, false);
+		}
 	}
 }
