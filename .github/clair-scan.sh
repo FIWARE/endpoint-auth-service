60ab233e352eb912d950986b3d6cfcaf1ffeadd3
Stefan Wiedemann <wistefan@googlemail.com>
2021-12-16 15:07:53 +0100

    -d

--- .github/clair-scan.sh
+++ .github/clair-scan.sh
@@ -4,7 +4,7 @@ failLevel=$1
 image=$2
 
 docker run -d --network host -p 5432:5432  --env POSTGRES_HOST_AUTH_METHOD=trust --env POSTGRES_DB=clair --env POSTGRES_USER=clair --env POSTGRES_PASSWORD=clair  docker.io/library/postgres:12
-docker run --network host -v $(pwd)/.github/config.yaml:/config/config.yaml quay.io/projectquay/clair:4.3.5
+docker run -d --network host -v $(pwd)/.github/config.yaml:/config/config.yaml quay.io/projectquay/clair:4.3.5
 wget https://github.com/quay/clair/releases/download/v4.3.5/clairctl-linux-amd64
 chmod +x clairctl-linux-amd64
 ./clairctl-linux-amd64 report --out json $image > clair.report
