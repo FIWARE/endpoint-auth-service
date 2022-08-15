0337a9a130ea30f9fe9e4f3fcdd32c6fbcff5a31
Stefan Wiedemann <stefan.wiedemann@fiware.org>
2022-08-15 15:19:25 +0200

    Auto stash before merge of "main" and "origin/main" (#34)
    * Auto stash before merge of "main" and "origin/main"
    
    * cleanup ci
    
    allow to ignore certain ports
    
    * put back and remove after merging the new ci
    
    * update listener yaml
    
    * update router url
    
    * fix initial config
    
    * fix test expectations

--- src/iptables-init/run.sh
+++ src/iptables-init/run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+for port in $(echo $PORTS_TO_IGNORE | tr "," "\n") 
+do
+    iptables -t nat -A OUTPUT -p tcp --dport $port -j RETURN
+done
+
 iptables -t nat -A OUTPUT -m owner --uid-owner $ENVOY_USER_ID -j RETURN
 iptables -t nat -A OUTPUT -p tcp -j REDIRECT --to-ports $ENVOY_PORT
