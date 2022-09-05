a95d345d09c6e92341ab85c3d0bc1154b7f190eb
Stefan Wiedemann <wistefan@googlemail.com>
2022-09-05 09:43:45 +0200

    try more

--- src/cached-auth-filter/main.go
+++ src/cached-auth-filter/main.go
@@ -311,7 +311,11 @@ func requestAuthProvider(authEntry EndpointAuthEntry, cas uint32) types.Action {
 	hs[methodIndex] = [2]string{":method", "GET"}
 	hs[pathIndex] = [2]string{":path", "/" + authEntry.AuthType + "/auth?domain=" + authEntry.Domain + "&path=" + authEntry.Path}
 	hs = append(hs, [2]string{"x-envoy-original-dst-host", config.AuthProviderAddress})
+	hs = append(hs, [2]string{":authority", config.AuthProviderAddress})
 
+	for _, h := range hs {
+		proxywasm.LogDebugf("Header: %v", h)
+	}
 	if _, err := proxywasm.DispatchHttpCall(config.AuthProviderName, hs, nil, nil, config.AuthRequestTimeout,
 		func(numHeaders, bodySize, numTrailers int) {
 			proxywasm.LogDebugf("Callback on auth-request response.")
