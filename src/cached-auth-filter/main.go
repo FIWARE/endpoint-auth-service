229d5a1e11ef66d18838a11265541386c38f72d0
Stefan Wiedemann <wistefan@googlemail.com>
2022-09-08 11:41:24 +0200

    try a long timeout

--- src/cached-auth-filter/main.go
+++ src/cached-auth-filter/main.go
@@ -296,7 +296,7 @@ func requestAuthProvider(authEntry EndpointAuthEntry, cas uint32) types.Action {
 	hs[methodIndex] = [2]string{":method", "GET"}
 	hs[pathIndex] = [2]string{":path", "/" + authEntry.AuthType + "/auth?domain=" + authEntry.Domain + "&path=" + authEntry.Path}
 
-	if _, err := proxywasm.DispatchHttpCall(config.AuthProviderName, hs, nil, nil, config.AuthRequestTimeout,
+	if _, err := proxywasm.DispatchHttpCall(config.AuthProviderName, hs, nil, nil, 30000,
 		func(numHeaders, bodySize, numTrailers int) {
 			proxywasm.LogDebugf("Callback on auth-request response.")
 			authCallback(authEntry, cas, bodySize)
